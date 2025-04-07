e8f5b7c7fbc64af9be5dac5839f4b9a8cde694f8
simeonKuran <simeon.kuran@gmx.at>
2025-04-07 16:40:46 +0200

    fix a critical typo

--- conan.cmake
+++ conan.cmake
@@ -199,7 +199,7 @@ macro(_conan_detect_compiler)
                     AND "${CMAKE_${LANGUAGE}_SIMULATE_ID}" STREQUAL "MSVC"))
 
         set(_VISUAL "msvc")
-	set(_VISUAL_VERSION, ${MSVC_VERSION})
+	set(_VISUAL_VERSION ${MSVC_VERSION})
  
         if("${_VISUAL_VERSION}" STREQUAL "")
             message(FATAL_ERROR "Conan: Visual Studio not recognized")
